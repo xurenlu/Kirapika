9d168fb9ab6ed5663bb5c053c46c497acbdcd577
Justin Jia <justin.jia.developer@gmail.com>
2013-05-16 18:25:46 +0800

    Initial Commit

---
+++ Kirapika/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Kirapika
+//
+//  Created by Justin Jia on 5/16/13.
+//  Copyright (c) 2013 Justin Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Kirapika/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Kirapika
+//
+//  Created by Justin Jia on 5/16/13.
+//  Copyright (c) 2013 Justin Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
